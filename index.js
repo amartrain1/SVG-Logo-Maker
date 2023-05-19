a598293255f717d420b903897f2b80730b2f9df9
Alex Martrain <alexander.martrain.2020@stpauls.com>
2023-05-19 14:47:56 -0500

    completed challenge

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const fs = require('fs');
 const { Square, Circle, Triangle } = require('./lib/shapes.js')
 
 logoMaker = (data) => {
-    if (data.shape == 'Square') {
-        const shape = new Square;
-        return shape.drawSquare()
+    const SVGOpen = `<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">`;
+    const SVGText = `<text x="150" y="120" font-size="65" text-anchor="middle" fill="${data.textColor}">${data.logoText}</text>`;
+    const triangleTxt = `<text x="150" y="150" font-size="65" text-anchor="middle" fill="${data.textColor}">${data.logoText}</text>`;
+    const SVGClose = `</svg>`;
+    if (data.logoShape == 'Square') {
+        const shape = new Square();
+        const generateCode = SVGOpen + shape.drawSquare(data.logoColor) + SVGText + SVGClose
+        return generateCode;
+    } else if (data.logoShape == 'Circle') {
+        const shape = new Circle();
+        const generateCode = SVGOpen + shape.drawCircle(data.logoColor) + SVGText + SVGClose
+        return generateCode;
+    } else {
+        const shape = new Triangle();
+        const generateCode = SVGOpen + shape.drawTriangle(data.logoColor) + triangleTxt + SVGClose
+        return generateCode;
     }
-}
+};
+
 
 inquirer
     .prompt([
@@ -16,7 +30,7 @@ inquirer
             type: 'input',
             name: 'logoText',
             message: 'Which letters would you like on your logo?',
-            // check to be sure the input is at least 1 character long, and not longer than 3 characters
+            // check to be sure the input is at least 1 character long, and no longer than 3 characters
             //! used ChatGPT to help understand how to use validate in inquirer prompts
             validate: function (value) {
                 if (value.length > 3) {
@@ -46,12 +60,11 @@ inquirer
             type: 'input',
             name: 'logoColor',
             message: 'What color would you like your logo to be? (input must be a keyword or a hexadecimal code)'
-        }
+        },
     ])
-    // .then((data) => console.log(data.logoShape))
     .then((data) => {
-        fs.writeFile('./example/logo.svg', 
-        ` <svg width="300" height="200" fill="white" version="1.1" xmlns="http://www.w3.org/2000/svg">
-        ${logoMaker(data.logoShape)}
-        </svg>`, (err) => err ? console.log(`Error: ${err}`) : console.log("File 'logo.svg' created successfully."))
+        // console.log('data.logoColor --- ' + data.logoColor),
+        // console.log('logoColor --- ' + logoColor)
+        // console.log(logoMaker(data))
+        fs.writeFile('./example/logo.svg', logoMaker(data), (err) => err ? console.log(`Error: ${err}`) : console.log("File 'logo.svg' created successfully."))
     });
